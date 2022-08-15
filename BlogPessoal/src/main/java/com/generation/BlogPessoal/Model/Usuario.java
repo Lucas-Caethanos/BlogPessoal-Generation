21b1e21ff30b98c6ecb47e507748b691199e5c7d
Lucas <lucascaetanocosta@gmail.com>
2022-08-15 17:29:55 -0300

    Adicionando spring-test no projeto

--- BlogPessoal/src/main/java/com/generation/BlogPessoal/Model/Usuario.java
+++ BlogPessoal/src/main/java/com/generation/BlogPessoal/Model/Usuario.java
@@ -21,8 +21,6 @@ public class Usuario {
     @Size(min = 3, max = 100)
     private String nome;
 
-    @Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
-    private String foto;
 
     @NotNull(message = "O atributo Usuário é Obrigatório!")
     @Email(message = "O atributo Usuário deve ser um email válido!")
@@ -32,9 +30,23 @@ public class Usuario {
     @Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
     private String senha;
 
+    @Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
+    private String foto;
+
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
     @JsonIgnoreProperties("usuario")
     private List<Postagem> postagem;
+
+    public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+        this.id = id;
+        this.nome = nome;
+        this.usuario = usuario;
+        this.senha = senha;
+        this.foto = foto;
+    }
+
+    public Usuario() {
+    }
 
     public Long getId() {
         return id;
